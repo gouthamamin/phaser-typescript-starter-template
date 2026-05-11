a5a302e5785a34cb2df64a77d37ccd8fd9d1a011
Goutham Amin <gouthamamin@gmail.com>
2026-05-12 00:34:52 +0530

    feat: update game configuration
    - update game resolution to portrait
    - update background color
    - add scale modes

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,24 +2,28 @@ import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 720,
+    height: 1080,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#000000',
     scene: [
         Boot,
         Preloader,
         MainMenu,
         MainGame,
         GameOver
-    ]
+    ],
+    scale:{
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    }
 };
 
 const StartGame = (parent: string) => {
